a8cad3622c328e5e0e7e5447ee6c12288c7591e6
prajolshakya <shakya_prajol@hotmail.com>
2018-04-09 11:46:11 -0500

    second commmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular 5 test area';
   firstname = 'Prajol';
-  last = 'Shakya';
+  lastname = 'Shakya';
 }
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+
+export class NavbarComponent {
+
+   status: boolean;
+
+
+  constructor() {
+    console.log('navbar constructor started');
+    this.getUserStatus();
+
+  }
+
+  getUserStatus() {
+    // make api call for db dall to set the value
+    this.status = true;
+  }
+
+  clickButton() {
+    alert('say hello');
+  }
+
+}
