1658d5924094ede7b2a666c023c14abd943dbbc7
prajolshakya <shakya_prajol@hotmail.com>
2018-04-10 10:29:28 -0500

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,26 @@
+import { ContactComponent } from './contact/contact.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponentComponent } from './home-component/home-component.component';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 
-// const appRoutes: Routes = [
-//   {path: '', component:}
+ const appRoutes: Routes = [
+   {path: '', component: HomeComponent},
+   {path: 'contact', component: ContactComponent}
 
-
-// ]
+ ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponentComponent,
-    HomeComponent,
-    ContactComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
